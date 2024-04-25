e981da9299825505e2c3cc865fb2a05ff96984f6
Hiiragi0831 <glazunovden@yandex.ru>
2024-04-25 12:33:54 +0300

    template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <Link href="/home">Home page</Link>
+        <main>
+            <p>dfdfd</p>
         </main>
     );
 }
